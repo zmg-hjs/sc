6cab314b0f3fe00370e6d9403df0228e5ad82726
huangjingshuai <511308194@qq.com>
2020-04-11 14:17:41 +0800

    页面完善

--- base/src/main/java/com/sc/base/entity/car/CarEntity.java
+++ base/src/main/java/com/sc/base/entity/car/CarEntity.java
@@ -26,9 +26,9 @@ public class CarEntity {
 
     private String userId; //拼车发起者id
     private String starting; //起始地
-    private String destinayion; //目的地
-    private String peopleNum; //目标人数
-    private String peopleNow; //现有人数
+    private String destination; //目的地
+    private int peopleNum; //目标人数
+    private int peopleNow; //现有人数
     private String telephone; //发起人电话
     private String carNum; //车牌号
     private String time; //出发时间
--- resident/src/main/java/com/sc/resident/service/car/CarService.java
+++ resident/src/main/java/com/sc/resident/service/car/CarService.java
@@ -1,9 +1,12 @@
 package com.sc.resident.service.car;
 
+import com.sc.base.dto.car.ManageCarIndexIntoDto;
 import com.sc.base.entity.car.CarEntity;
 import com.sc.base.repository.car.CarRepository;
+import myString.MyStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import vo.Result;
 
 /**
  * Package: com.sc.resident.service.car
@@ -19,7 +22,34 @@ public class CarService {
 
     @Autowired
     private CarRepository carRepository;
+
+    /**
+     * 查询拼车信息(通过拼车id)
+     * @param id
+     * @return
+     */
     public CarEntity findCarEnityById(String id){
         return carRepository.findCarEntityById(id);
+    }
+
+    /**
+     * 添加拼车信息
+     * @param manageCarIndexIntoDto
+     * @return
+     */
+    public Result addCarEntity(ManageCarIndexIntoDto manageCarIndexIntoDto){
+
+        CarEntity carEntity = new CarEntity();
+        carEntity.setId(MyStringUtils.getIdDateStr("residentCar"));
+        carEntity.setCarNum(manageCarIndexIntoDto.getCarNum());
+        carEntity.setDestination(manageCarIndexIntoDto.getDestination());
+        carEntity.setTime(manageCarIndexIntoDto.getTime());
+        carEntity.setStarting(manageCarIndexIntoDto.getStarting());
+        carEntity.setTelephone(manageCarIndexIntoDto.getTelephone());
+        carEntity.setPeopleNum(manageCarIndexIntoDto.getPeopleNum());
+        carEntity.setUserId(manageCarIndexIntoDto.getUserId());
+        System.out.print(carEntity);
+        carRepository.saveAndFlush(carEntity);;
+        return  new Result().setSuccess(carEntity);
     }
 }
