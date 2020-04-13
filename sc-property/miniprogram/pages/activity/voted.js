a47e1c1e9d7b5f2bd92093414c537707b5ec6ecf
huangjingshuai <511308194@qq.com>
2020-04-14 00:26:35 +0800

    api物业端测试

--- sc-property/miniprogram/pages/activity/voted.js
+++ sc-property/miniprogram/pages/activity/voted.js
@@ -6,13 +6,29 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    list:[],
+    title:''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var that=this
+    wx.request({
+      url: activityUrl+'/enroll/findResult',
+      method:'POST',
+      data:{
+        activityId:options.id
+      },
+      success:function(res){
+         that.setData({
+           list:res.data.data,
+           title:options.title
+         })
+      }
+    })
+
 
   },
 
