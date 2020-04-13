5c519e4c459afe8d0cb240749716fb11e5bcadfe
huangjingshuai <511308194@qq.com>
2020-04-13 11:25:10 +0800

    api1.1

---
+++ sc-resident/pages/car/history.js
@@ -0,0 +1,94 @@
+// pages/car/history.js
+const carUrl=require('../../config').carUrl
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list:[]
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that=this
+     wx.request({
+       url: carUrl+'my',
+       method:'POST',
+       data:{
+        carpoolStatus:options.carpoolStatus,
+        userId:wx.getStorageSync('userInfo').id
+       },
+       success:function(res){
+          that.setData({
+            list:res.data.data
+          })
+          console.log(that.data.list)
+       }
+     })
+     if(this.data.list==''){
+       console.log('jajaj')
+      wx.showModal({
+        title: '提示',
+        content: '您还未拼过车',
+        showCancel: false,
+        confirmText: "确定",
+        success:function(res){
+          wx.navigateBack()
+        }
+     })
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
