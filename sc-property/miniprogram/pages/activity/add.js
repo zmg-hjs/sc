ca511a6ccdf9544e71b616c3944ddd638d1dd767
huangjingshuai <511308194@qq.com>
2020-04-14 13:06:35 +0800

    修复

--- sc-property/miniprogram/pages/activity/add.js
+++ sc-property/miniprogram/pages/activity/add.js
@@ -161,9 +161,9 @@ Page({
         content: that.data.content,
         committeesNumber:e.detail.value.committeesNumber,
         hostParty:that.data.info.name,
-        activityStartTimeStr:that.data.info.actStrD+" "+that.data.info.actStrT+":00",
+        activityStartTimeStr:that.data.info.actStaD+" "+that.data.info.actStaT+":00",
         activityEndTimeStr:that.data.info.actEndD+" "+that.data.info.actEndT+":00",
-        votingStartTimeStr:that.data.info.votStrD+" "+that.data.info.votStrT+":00",
+        votingStartTimeStr:that.data.info.votStaD+" "+that.data.info.votStaT+":00",
         votingEndTimeStr:that.data.info.votEndD+" "+that.data.info.votEndT+":00"
 
         
